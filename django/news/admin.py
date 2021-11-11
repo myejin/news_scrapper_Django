b306bdea2b0b491c0a941930e27b58597e7b2501
kgw012 <kgw012@gmail.com>
2021-11-11 19:35:15 +0900

    211111 keyword admin add

--- django/news/admin.py
+++ django/news/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Article, Company, FollowCompany
+from .models import Article, Company, FollowCompany, Keyword
 
 admin.site.register(Article)
 admin.site.register(Company)
 admin.site.register(FollowCompany)
+admin.site.register(Keyword)
